8d3b0d484a4bc9758370ba2b627c1349eb7bff66
Kenny 'Hulk Smash' Webb <kenny@zero2one.co.za>
2019-01-27 01:34:31 +0200

    New Front and Testing

---
+++ cloudfront/src/App.vue
@@ -0,0 +1,85 @@
+<template>
+  <div id="app">
+    <div class="container-fluid">
+      <div class='row'>
+        <div class="col-md-8">
+          <b-card title="Bucket Status"
+              tag="article"
+              class="mb-2">
+            <p class="card-text">
+              Data is gonna go here soon
+            </p>
+            <b-button href="#" variant="primary">Refresh</b-button>
+          </b-card>
+          <b-card title="Dropzone"
+              tag="article"
+              class="mb-2">
+            <p class="card-text">
+              Drop your files here
+            </p>
+            <vue-dropzone ref="myVueDropzone" id="dropzone" :options="dropzoneOptions"></vue-dropzone>
+          </b-card>
+        </div>
+        <div class="col-md-4">
+          <b-card title="Global Controls"
+              tag="article"
+              class="mb-2">
+            <b-button href="#" variant="primary">Start Upload</b-button>&nbsp;
+            <b-button href="#" variant="info">Pause All</b-button>&nbsp;
+            <b-button href="#" variant="danger">Cancel All</b-button>&nbsp;
+            <b-button href="#" variant="warning">Tidy List</b-button>
+          </b-card>
+          <b-card class="mb-2">
+            
+          </b-card>
+        </div>
+      </div>
+        
+      
+    </div>
+  </div>
+</template>
+
+<script>
+import vue2Dropzone from 'vue2-dropzone';
+
+export default {
+  name: 'app',
+  components: {
+    vueDropzone: vue2Dropzone
+  },
+  methods: {
+       
+  },
+  data: function () {
+    return {
+      myFiles: {},
+      dropzoneOptions: {
+        url: 'https://linkstorm-res-test.herokuapp.com/resumable',
+        thumbnailWidth: 150,
+        query: { path: '10/1' },
+        fileType: ['gif', 'jpg', 'png', 'jpeg'],
+        chunkSize: 128*1024,
+        simultaneousUploads: 100,
+        setChunkTypeFromFile: true,
+        prioritizeFirstAndLastChunk: true,
+        parallelChunkUploads: true
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../node_modules/bootstrap-vue/dist/bootstrap-vue.css';
+@import '../node_modules/bootstrap/scss/bootstrap.scss';
+@import '../node_modules/vue2-dropzone/dist/vue2Dropzone.min.css';
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,8 @@
 $(document).ready( function() {
-  var zone = $('.dropzone').ṛesumable({
-    query: { path: '10/1' },
-    target: 'https://linkstorm-res-test.herokuapp.com/resumable',
-    fileType: ['gif', 'jpg', 'png', 'jpeg'],
-    chunkSize: 128*1024,
-    simultaneousUploads: 100,
-    setChunkTypeFromFile: true,
-    prioritizeFirstAndLastChunk: true
-  });
-
   axios.get('/bucketStatus')
     .then(function(response){
       console.log('status GET', response);
     }).catch( function (error){
       console.error(error);
-    })
+    });
 });
