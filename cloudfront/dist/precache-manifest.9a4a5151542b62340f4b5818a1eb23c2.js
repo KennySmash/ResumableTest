468d884a1e5972efe6b41f88cd8d6c67eea1e978
Kenny 'Hulk Smash' Webb <kenny@zero2one.co.za>
2019-01-27 11:35:35 +0200

    Build Deployed

---
+++ cloudfront/dist/precache-manifest.9a4a5151542b62340f4b5818a1eb23c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "6f017807b9768d3dfa48",
+    "url": "/js/chunk-vendors.86369071.js"
+  },
+  {
+    "revision": "ec4af85b914fe3ac3775",
+    "url": "/js/app.04819618.js"
+  },
+  {
+    "revision": "7f0bf4bf6d9fbd4686bf6217bf7c415f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f017807b9768d3dfa48",
+    "url": "/css/chunk-vendors.c54fc4d2.css"
+  },
+  {
+    "revision": "ec4af85b914fe3ac3775",
+    "url": "/css/app.2bfd449e.css"
+  }
+];
