cf30f8c4fb3376ba6a4de28f8c50f0d33703d718
Kenny 'Hulk Smash' Webb <kenny@zero2one.co.za>
2019-02-03 14:26:10 +0200

    removed old public

--- public/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready( function() {
-  axios.get('/bucketStatus')
-    .then(function(response){
-      console.log('status GET', response);
-    }).catch( function (error){
-      console.error(error);
-    });
-});
